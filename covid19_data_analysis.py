3cb929510aa98c2d7a4d7e7ac90a7b7c8a442d98
bandyopi <indranil19@yahoo.com>
2020-04-03 18:35:50 +0100

    Refactoring

--- covid19_data_analysis.py
+++ covid19_data_analysis.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv("C:\\data\\covid-19\\csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_deaths_global.csv")
+df = pd.read_csv("D:\\Data\\covid-19\\csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_deaths_global.csv")
+num_days, top_n = 5, 2
+
+# Find the latest date and short by the lates date desc
 latest_date = df.columns[-1]
 df.sort_values(by=[latest_date], ascending=False, inplace=True)
+
+# Find the last n columns
+last_col = df.columns.size
+last_n_col = df.columns[1:2].append(df.columns[last_col-num_days:last_col])
+print(last_n_col)
+
 print("Top 10 countries by death asOf: " + latest_date )
-print(list("Country/Region").append(df.columns[df.columns.size-5:df.columns.size]))
-print(df[["Country/Region", df.columns[-5], df.columns[-4], df.columns[-3], df.columns[-2], latest_date]].head(15))
+print(df[last_n_col].head(15))
 
