b6a00db4ec102c0a804e95bdd0d6299ee552f809
bandyopi <indranil19@yahoo.com>
2020-04-06 10:52:15 +0100

    Updated sorting to last n day average

--- covid19_data_analysis.py
+++ covid19_data_analysis.py
@@ -3,13 +3,14 @@ import pandas as pd
 num_days, top_n = 5, 25
 df = pd.read_csv("csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_deaths_global.csv")
 
-# Find the latest mortality, sort descending and take top_n
-df['increase'] = df[df.columns[-1]] - df[df.columns[-2]]
-df.sort_values(by=['increase'], ascending=False, inplace=True)
+# Find the last n-day average mortality, sort descending and take top_n
+n_day_avg = str(num_days)+'DayAvg'
+df[n_day_avg] = (df[df.columns[-1]] - df[df.columns[-1-num_days]])/num_days
+df.sort_values(by=[n_day_avg], ascending=False, inplace=True)
 df=df.head(top_n)
 
 # Update the last n columns with mortality increase from previous date
-cols = ['Country/Region']
+cols = ['Country/Region', n_day_avg]
 for i in range(2,num_days+2):
     dt, prev_dt = df.columns[-i], df.columns[-i-1]
     df[dt] = df[dt].map(str) + ' (' + (df[dt] - df[prev_dt]).map(str) + ')'
