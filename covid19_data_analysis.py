3c4cb353da3bb00de886bfbe1ad949ec40335e02
bandyopi <indranil19@yahoo.com>
2020-04-06 23:17:41 +0100

    Added GroupBy clause in the DataFrame as some countries are split in regions (e.g. Australia)

--- covid19_data_analysis.py
+++ covid19_data_analysis.py
@@ -2,12 +2,14 @@ import pandas as pd
 
 num_days, top_n = 5, 25
 df = pd.read_csv("csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_deaths_global.csv")
+df = df.groupby(by=['Country/Region'], as_index=False).sum()
+#df = df[df['Country/Region']=='Australia'] # Set the filter to get a specific Country
 
 # Find the last n-day average mortality, sort descending and take top_n
 n_day_avg = str(num_days)+'DayAvg'
 df[n_day_avg] = (df[df.columns[-1]] - df[df.columns[-1-num_days]])/num_days
 df.sort_values(by=[n_day_avg], ascending=False, inplace=True)
-df=df.head(top_n)
+df = df.head(top_n)
 
 # Update the last n columns with mortality increase from previous date
 cols = ['Country/Region', n_day_avg]
