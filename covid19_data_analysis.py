800de90bb932254501068e397fddbdaff4a39ee8
bandyopi <indranil19@yahoo.com>
2020-04-09 21:09:34 +0100

    Misc refactoring

--- covid19_data_analysis.py
+++ covid19_data_analysis.py
@@ -1,9 +1,10 @@
-import pandas as pd
+import pandas as pd, matplotlib.pyplot as plot
 
-num_days, top_n = 5, 25
+num_days, top_n = 5, 20
 df = pd.read_csv("csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_deaths_global.csv")
-df = df.groupby(by=['Country/Region'], as_index=False).sum()
 #df = df[df['Country/Region']=='Australia'] # Set the filter to get a specific Country
+df = df.groupby(by=['Country/Region'], as_index=False).sum()
+total_mortality = df[df.columns[-1]].sum()
 
 # Find the last n-day average mortality, sort descending and take top_n
 n_day_avg = str(num_days)+'DayAvg'
@@ -15,6 +16,10 @@ df = df.head(top_n)
 cols = ['Country/Region', n_day_avg]
 for i in range(2,num_days+2):
     dt, prev_dt = df.columns[-i], df.columns[-i-1]
-    df[dt] = df[dt].map(str) + ' (' + (df[dt] - df[prev_dt]).map(str) + ')'
+    df[dt] = df[dt].map(str) + ' [+' + (df[dt] - df[prev_dt]).map(str) + ']'
     cols.append(dt)
+print('Displaying', num_days, 'days stats for top', top_n, 'countries by mortality. Total', 
+    total_mortality, 'reported mortalities asOf', df.columns[-2])
 print(df[cols])
+#ax=df.plot.bar(x='Country/Region', y=n_day_avg, rot=0)
+#plot.show()
