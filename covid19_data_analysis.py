5e2b8605dc4d3e14cceb0214e96592b17d60230e
bandyopi <indranil19@yahoo.com>
2020-04-10 10:19:48 +0100

    Added Mortality SMA Plot for top 5 countries

--- covid19_data_analysis.py
+++ covid19_data_analysis.py
@@ -9,32 +9,43 @@ dt_latest = df.columns[-1]
 total_mortality = df[dt_latest].sum()
 
 # Find the last n-day moving average mortality for the last n days, sort descending and take top_n
-n_day_avg = str(num_days)+'DaySMA-'+dt_latest
+n_day_avg = str(num_days)+'DayAvg'
 df[n_day_avg] = (df[dt_latest] - df[df.columns[-1-num_days]])/num_days
 df.sort_values(by=[n_day_avg], ascending=False, inplace=True)
 df = df.head(top_n)
 df_sma = pd.DataFrame()
 df_sma[country] = df[country]
 df_sma[dt_latest] = df[n_day_avg]
-for i in range(2, num_days+2):
+for i in range(2, 16): #num_days+2):
     dt, dt_prev = df.columns[-i], df.columns[-i-num_days]
     df_sma[dt] = (df[dt] - df[dt_prev])/num_days
 #print(df_sma)
 
 # Update the last n columns with mortality increase from previous date
-cols = [country]
+cols = [country, n_day_avg]
 for i in range(2,num_days+2):
     dt, dt_prev = df.columns[-i], df.columns[-i-1]
     df[dt] = df[dt].map(str) + ' [+' + (df[dt] - df[dt_prev]).map(str) + ']'
     cols.append(dt)
 print('Displaying last', num_days, 'days stats for top', top_n, 'countries by mortality. Total', 
     total_mortality, 'reported mortalities asOf', dt_latest)
-print(pd.concat([df[cols], df_sma], axis=1))
+print(df[cols])
+#print(pd.concat([df[cols], df_sma], axis=1))
 
 # Plot the SMA for top 5 countries
-df_plt = df_sma.head(5)
-print(df_plt)
+  # Need to set the dataframe index to 'country' 
+  #  so the dataframe can be transposed with 'country' becoming column header instead of numeric indexes in df_sma
+  # The transpose() 'T' is needed  because dataframe plots line series per column 
+  # The iloc[::-1] is to reverse the row ordering (date shorting)
+df_plt = df_sma.head(5).set_index(country).T.iloc[::-1]
+#print(df_plt)
+df_plt.plot(kind='line')
 ax = plt.gca()
-for i in range(1, num_days+1):
-    df_sma.head(5).plot(kind='line', x=country, y=df_plt.columns[-i], ax=ax)
+#ax.set_xlabels(df_plt.index)
+#for i in range(1, len(df.index)+1):
+ #   df_plt.plot(kind='line', y=df_plt.columns[-i], x=country, ax=ax)
+#plt.xscale(df_plt.index)
+plt.title('5 day moving agerage mortality by top 5 countroes')
+plt.xlabel('Date ->')
+plt.ylabel('Mortality (5 Days SMA) ->')
 plt.show()
